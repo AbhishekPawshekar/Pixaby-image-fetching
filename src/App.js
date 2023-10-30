fae7bb4bd0b72e9baf8139ea9f55d47539b3f24d
mrunalpawshekar99 <mrunal.pawshekar@predapp.de>
2023-10-30 21:51:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+function App () {
+          const[data,setdata]=useState([]);
+          const[search,setsearch]=useState("");
+          
+          useEffect(()=>{
+                    fetch(`https://pixabay.com/api/?key=40363710-adb7f100bb05e58b699049dc7&q=${search}&image_type=photo`)
+                    .then(result=>result.json()).then(d=>setdata(d.hits));
+          },[data]);
+          
+          
+          return (
+    <div className="pixabay_app">
+         <header className='Pixaby_header' >
+            <nav className='Pixaby_nav'>
+                  <div><i className={"fa-solid fa-camera"}></i><h1>PIXABY</h1></div>
+                 <div> Explore 
+                  <i className={"fa-solid fa-chevron-down"}></i>
+                  
+                  <i className={"fa-solid fa-bell"}></i>
+                  <button className='upload_button'>Upload</button>
+                  </div>
+                  
+            </nav>
+            <main className='pixaby_main'>
+                  <h1>Stunning royalty-free images & royalty-free stock</h1>
+                 <div>
+                   <h3>Over 4.1 million+ high quality stock images,videos and music shared by our talented community.</h3>
+                  <input type="text" onKeyUp={(x)=>{setsearch(x.target.value)}} className='search_input' placeholder='Search image here. What do you want.'/>
+            </div>
+            </main>
+            
+         </header>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className='fetching_img'>
+          {data.map((e)=>{
+            return(
+                  <section key={e.id}>
+                    <img src={e.webformatURL} alt=""/>
+                  </section>
+               
+            )
+          })}
+          </div>
+         
+    
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
